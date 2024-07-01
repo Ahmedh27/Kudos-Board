b349cab589c50acb825fa594a4c6c2a9ebbc13ae
larnelle15 <larnelleankunda50@gmail.com>
2024-07-01 14:57:17 -0700

    Created Header, footer, Sub nav bar and Not found component

---
+++ kudo-board-ui/src/Components/NotFound/NotFound.jsx
@@ -0,0 +1,14 @@
+import "./NotFound.css"
+
+function NotFound({ user }) {
+  return (
+    <div className="NotFound">
+      <div className="cta">
+        <h1>404</h1>
+        <p>That page does not exist</p>
+      </div>
+    </div>
+  )
+}
+
+export default NotFound;
---
+++ kudo-board-ui/src/Components/SubNavBar/SubNavBar.jsx
@@ -0,0 +1,41 @@
+import "./SubNavBar.css"
+
+function SubNavbar({ activeCategory, setActiveCategory, searchInputValue, handleOnSearchInputChange }) {
+
+
+  const categories = ["All", "Recent", "Celebration", "Thank You", "Inspiration"];
+
+  return (
+    <nav className="SubNavbar">
+
+      <div className="content">
+
+        <div className="row">
+          <div className="search-bar">
+            <input
+              type="text"
+              name="search"
+              placeholder="Search"
+              value={searchInputValue}
+              onChange={handleOnSearchInputChange}
+            />
+            <i className="material-icons">search</i>
+          </div>
+        </div>
+
+        <div className="row">
+          <ul className={`category-menu`}>
+            {categories.map((cat) => (
+              <li className={activeCategory === cat ? "is-active" : ""} key={cat}>
+                <button onClick={() => setActiveCategory(cat)}>{cat}</button>
+              </li>
+            ))}
+          </ul>
+        </div>
+        
+      </div>
+    </nav>
+  )
+}
+
+export default SubNavbar;
